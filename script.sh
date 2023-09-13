d128461841a4dd9684767aeadfdf408d5980454a
Pavel Belov <pasha_belov_96@mail.ru>
2023-09-13 22:16:12 +0300

    update script file for wordpress

--- script.sh
+++ script.sh
@@ -2,46 +2,30 @@
 
 
 #create 3 wordpress/add database/add change color in wordpress 
+
+DB_NAMES=(wp_db1 wp_db2 wp_db3)
+DB_USERS=(wp_user1 wp_user2 wp_user3)
 hosts=(site1.com site2.com site3.com)
-path1="/var/www/${hosts[0]}"
-path2="/var/www/${hosts[1]}"
-path3="/var/www/${hosts[2]}"
-style_file1=$path1/wp-admin/css/colors/light/colors-rtl.css
-style_file2=$path2/wp-admin/css/colors/light/colors-rtl.css
-style_file3=$path3/wp-admin/css/colors/light/colors-rtl.css
-redcolor="#fa0505"
-greencolor="##16ed0e"
-yellowcolor="#c5f005"
+colors=("#FF0000" "#00FF00" "#0000FF")
+path="/var/www"
+default_color="#f5f5f5"
+default_path_color="wp-admin/css/colors/light/colors-rtl.css"
+wp_content="wp-content/uploads"
+wp_config_simple="wp-config-sample.php"
+wp_config="wp-config.php"
 
-cd /tmp && wget https://wordpress.org/latest.tar.gz
-sudo tar -xvf /tmp/latest.tar.gz -C /var/www
-sudo mv /var/www/wordpress "$path1"
-sudo cp -r "$path1" "$path2"
-sudo cp -r "$path1" "$path3"
-sudo chown -R www-data:www-data /var/www/
-sudo chmod -R 755 /var/www/
-sudo mkdir $path1/wp-content/uploads
-sudo mkdir $path2/wp-content/uploads
-sudo mkdir $path3/wp-content/uploads
-sudo chown -R www-data:www-data $path1/wp-content/uploads/
-sudo chown -R www-data:www-data $path2/wp-content/uploads/
-sudo chown -R www-data:www-data $path3/wp-content/uploads/
+wget https://wordpress.org/latest.tar.gz -P /tmp/
+sudo tar -xvf /tmp/latest.tar.gz -C $path
 
-sudo sed -i "s/background: #f5f5f5;/background: ${redcolor};/g" $style_file1
-sudo sed -i "s/background: #f5f5f5;/background: ${greencolor};/g" $style_file2
-sudo sed -i "s/background: #f5f5f5;/background: ${yellowcolor};/g" $style_file3
+for (( i = 0; i < "${#hosts[*]}"; i++ ))
+do
+  cp -r $path/wordpress $path/${hosts[i]}
+  mkdir $path/${hosts[i]}/$wp_content
+  cp $path/${hosts[i]}/$wp_config_simple $path/${hosts[i]}/$wp_config 
+  sudo sed -i "s/database_name_here/${DB_NAMES[i]}/" $path/${hosts[i]}/$wp_config
+  sudo sed -i "s/username_here/${DB_USERS[i]}/" $path/${hosts[i]}/$wp_config
+  sudo sed -i "s/background: $default_color;/background: ${colors[i]};/g" $path/${hosts[i]}/$default_path_color
+done
 
-sudo cp $path1/wp-config-sample.php $path1/wp-config.php 
-sudo sed -i "s/database_name_here/${name_wp}/" $path1/wp-config.php
-sudo sed -i "s/username_here/${name_wp}/" $path1/wp-config.php
-sudo sed -i "s/password_here/${pass_wp}/" $path1/wp-config.php
-
-sudo cp $path2/wp-config-sample.php $path2/wp-config.php 
-sudo sed -i "s/database_name_here/${name_wp}/" $path2/wp-config.php
-sudo sed -i "s/username_here/${name_wp}/" $path2/wp-config.php
-sudo sed -i "s/password_here/${pass_wp}/" $path2/wp-config.php
-
-sudo cp $path3/wp-config-sample.php $path3/wp-config.php 
-sudo sed -i "s/database_name_here/${name_wp}/" $path3/wp-config.php
-sudo sed -i "s/username_here/${name_wp}/" $path3/wp-config.php
-sudo sed -i "s/password_here/${pass_wp}/" $path3/wp-config.php
+sudo chown -R www-data:www-data $path/
+sudo chmod -R 755 $path/
